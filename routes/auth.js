bf370c4ae953db633d03a74b9d9ca1fd8968c3e9
Punit9349 <puneetgarg9349@gmail.com>
2024-09-18 21:17:59 +0530

    removing a small glitch

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const User = require('../models/User');
+const User = require('../models/user');
 const Student=require('../models/student')
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
